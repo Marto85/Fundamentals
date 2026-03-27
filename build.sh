8f72e0c776544efdbe59ddbf758cf19fc844c46f
MartoJuanDev <martojuandev@gmail.com>
2026-03-26 22:53:29 -0300

    cambio en el build

--- build.sh
+++ build.sh
@@ -4,12 +4,10 @@ set -e
 echo "▶ Installing frontend dependencies..."
 cd frontend
 npm install
-npm run build
+npx vite build
 cd ..
 
 echo "▶ Installing backend dependencies..."
-cd backend
 pip install -r requirements.txt
-cd ..
 
 echo "✅ Build complete"
