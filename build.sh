358a85b199afd302282d51d7dc4f3c4a65cf885c
MartoJuanDev <martojuandev@gmail.com>
2026-03-26 23:03:31 -0300

    agrego frontend add para deploy

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo "▶ Installing frontend dependencies..."
-cd frontend
-npm install
-npx vite build
-cd ..
-
 echo "▶ Installing backend dependencies..."
 pip install -r requirements.txt
 
